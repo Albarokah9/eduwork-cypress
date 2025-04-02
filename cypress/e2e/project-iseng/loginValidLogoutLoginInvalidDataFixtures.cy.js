bda92d66bca341b6a2bc1b8972c62c5535fbd85d
Albarokah Rifansah <albareizy942@gmail.com>
2025-04-02 16:25:35 +0700

    update-file

---
+++ cypress/e2e/project-iseng/loginValidLogoutLoginInvalidDataFixtures.cy.js
@@ -0,0 +1,34 @@
+
+describe('Login dengan fixtures data', () => {
+    beforeEach(() => {
+        cy.visit('http://zero.webappsecurity.com/login.html');
+
+        // Mengambil data dari fixture file dua.json
+        cy.fixture("dua").then((data) => {
+            const username = data.validUser.username;
+            const password = data.validUser.password;
+
+            // Mengisi form login
+            cy.get('#user_login').type("username");
+            cy.get('#user_password').type("password");
+            cy.contains('Sign in').click();
+
+            // Assertion: Memastikan login berhasil
+            cy.url().should('not.include', 'login.html');
+            cy.get('.nav-tabs').should('contain', 'Account Summary');
+        });
+    });
+
+    it('Melakukan Logout', () => {
+        // Logout 
+        cy.get('.icon-user').click();  // klik ikon user
+        cy.contains('Logout').click();  // klik tombol logout
+
+        // Assertion: Memastikan logout berhasil
+        cy.url().should('include', 'index.html');
+        cy.get('.icon-signin', { timeout: 10000 }).should('be.visible');
+        
+    });
+
+    
+});
