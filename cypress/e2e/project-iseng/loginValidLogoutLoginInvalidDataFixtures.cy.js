e2cec8b4a4bd6937653eba9dc3475c32aa941b92
Albarokah Rifansah <albareizy942@gmail.com>
2025-04-02 23:17:16 +0700

    update-file

--- cypress/e2e/project-iseng/loginValidLogoutLoginInvalidDataFixtures.cy.js
+++ cypress/e2e/project-iseng/loginValidLogoutLoginInvalidDataFixtures.cy.js
@@ -1,34 +1,32 @@
 
-describe('Login dengan fixtures data', () => {
+describe('Login dengan valid fixtures data', () => {
     beforeEach(() => {
-        cy.visit('http://zero.webappsecurity.com/login.html');
+        cy.visit('http://zero.webappsecurity.com/login.html')
 
         // Mengambil data dari fixture file dua.json
         cy.fixture("dua").then((data) => {
-            const username = data.validUser.username;
-            const password = data.validUser.password;
+            const username = data.validUser.username
+            const password = data.validUser.password
 
             // Mengisi form login
-            cy.get('#user_login').type("username");
-            cy.get('#user_password').type("password");
-            cy.contains('Sign in').click();
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').type(password)
+            cy.contains('Sign in').click()
 
             // Assertion: Memastikan login berhasil
-            cy.url().should('not.include', 'login.html');
-            cy.get('.nav-tabs').should('contain', 'Account Summary');
+            cy.url().should('not.include', 'login.html')
+            cy.get('.nav-tabs').should('contain', 'Account Summary')
         });
     });
 
     it('Melakukan Logout', () => {
         // Logout 
-        cy.get('.icon-user').click();  // klik ikon user
-        cy.contains('Logout').click();  // klik tombol logout
+        cy.get('.icon-user').click()  // klik ikon user
+        cy.contains('Logout').click()  // klik tombol logout
 
         // Assertion: Memastikan logout berhasil
-        cy.url().should('include', 'index.html');
-        cy.get('.icon-signin', { timeout: 10000 }).should('be.visible');
+        cy.url().should('include', 'index.html')
+        cy.get('#signin_button').should('be.visible')
         
-    });
-
-    
+    }); 
 });
