05b7e226a717c50711fde984134fa886eebd68ab
Albarokah Rifansah <albareizy942@gmail.com>
2025-04-03 15:47:41 +0700

    update-file

--- cypress/e2e/project-iseng/iseng2.cy.js
+++ cypress/e2e/project-iseng/iseng2.cy.js
@@ -1,20 +1,20 @@
-describe("Should login succes with validUser", () => {
+describe("SauceDemo Login and Logout", () => {
     beforeEach(() => {
-      cy.visit("https://www.saucedemo.com/");
-    })
+      cy.visit("https://www.saucedemo.com/")
+    });
   
-    it("Should login successfully with validUser", () => {
+    it("Login Success", () => {
       cy.fixture("saucedemo").then((users) => {
+        // Menggunakan data validUser dari fixture
         cy.get('[data-test="username"]').type(users.validUser.username)
         cy.get('[data-test="password"]').type(users.validUser.password)
         cy.get('[data-test="login-button"]').click()
         
-        // cy.url().should("contain", "/inventory.html"); menggunakan contain untuk melakukan pemeriksaan sebagian artinya URL cukup mengandung bagian /intentory.html
-        cy.url().should("eq", "https://www.saucedemo.com/inventory.html"); 
-        // eq(equal): untuk melakukan pemeriksaan sama persis, URL harus 100% sama persis dengan https://www.saucedemo.com/inventory.html
+        // Verifikasi bahwa login berhasil dan berada di halaman Inventory
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
       })
     })
-
+  
     it("Logout Success", () => {
       cy.fixture("saucedemo").then((users) => {
         // Login terlebih dahulu
@@ -34,20 +34,16 @@ describe("Should login succes with validUser", () => {
       })
     })
   
-    it("Should fail to login with invalidUser", () => {
+    it("Login Failed with Invalid User", () => {
       cy.fixture("saucedemo").then((users) => {
+        // Menggunakan data invalidUser dari fixture
         cy.get('[data-test="username"]').type(users.invalidUser.username)
         cy.get('[data-test="password"]').type(users.invalidUser.password)
         cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="error"]').should("contain.text", "Epic sadface: Sorry, this user has been locked out.");
+  
+        // Verifikasi bahwa error message muncul
+        cy.get('[data-test="error"]').should("contain.text", "Epic sadface: Sorry, this user has been locked out.")
       })
     })
   })
   
-// Atribut	Format Cypress	Contoh
-// id=	    #idName	        cy.get('#user-name')
-// class=	  .className	    cy.get('.btn-primary')
-// type=	  [type="value"]	cy.get('[type="text"]')
-
-// Atribut lain	[atribut="value"]	cy.get('[data-test="login-button"]')
-// Gabungan	tagname#idName.className[type="value"]	cy.get('input#user-name[type="text"]')
--- cypress/e2e/project-iseng/project.zerowebappsecurity.cy.js
+++ cypress/e2e/project-iseng/project.zerowebappsecurity.cy.js
@@ -2,25 +2,25 @@ describe('Automation Login dengan kredensial invalid dan valid menggunakan fixtu
 
     beforeEach(() => {
         // Mengunjungi halaman login sebelum setiap tes
-        cy.visit('http://zero.webappsecurity.com/login.html');
-    });
+        cy.visit('http://zero.webappsecurity.com/login.html')
+    })
 
     it('Login dengan kredensial tidak valid', () => {
         // Mengambil data dari fixture file dua.json
         cy.fixture("dua").then((data) => {
-            const username = data.invalidUser.username;
-            const password = data.invalidUser.password;
+            const username = data.invalidUser.username
+            const password = data.invalidUser.password
 
             // Mengisi form login dengan kredensial tidak valid
-            cy.get('#user_login').type(username);
-            cy.get('#user_password').type(password);
-            cy.contains('Sign in').click();
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').type(password)
+            cy.contains('Sign in').click()
 
             // Assertion: Memastikan muncul pesan kesalahan
             cy.get('.alert-error').should('be.visible')
-                .and('contain', 'Login and/or password are wrong.');
-        });
-    });
+                .and('contain', 'Login and/or password are wrong.')
+        })
+    })
 
     it('Login dengan kredensial valid dan logout', () => {
         // Mengambil data dari fixture file dua.json
@@ -29,22 +29,22 @@ describe('Automation Login dengan kredensial invalid dan valid menggunakan fixtu
             const password = data.validUser.password;
 
             // Mengisi form login dengan kredensial valid
-            cy.get('#user_login').type(username);
-            cy.get('#user_password').type(password);
-            cy.contains('Sign in').click();
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').type(password)
+            cy.contains('Sign in').click()
 
             // Assertion: Memastikan login berhasil
-            cy.url().should('not.include', 'login.html');
-            cy.get('.nav-tabs').should('contain', 'Account Summary');
+            cy.url().should('not.include', 'login.html')
+            cy.get('.nav-tabs').should('contain', 'Account Summary')
 
             // Logout dari aplikasi
-            cy.get('.icon-user').click();  // Klik ikon user
-            cy.contains('Logout').click();  // Klik tombol logout
+            cy.get('.icon-user').click()  // Klik ikon user
+            cy.contains('Logout').click()  // Klik tombol logout
 
             // Assertion: Memastikan logout berhasil
-            cy.url().should('include', 'index.html');
-            cy.get('#signin_button').should('be.visible');
-        });
-    });
+            cy.url().should('include', 'index.html')
+            cy.get('#signin_button').should('be.visible')
+        })
+    })
 
-});
+})
